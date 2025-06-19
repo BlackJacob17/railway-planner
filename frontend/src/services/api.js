a44e0de45e1ffebbcfec303e88c78f601e28534b
BlackJacob <prabhav.tomar.ug22@nsut.ac.in>
2025-06-20 04:32:04 +0530

    Update API URLs to use environment variables

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAuthToken } from '../utils/auth';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Create axios instance
 const api = axios.create({
