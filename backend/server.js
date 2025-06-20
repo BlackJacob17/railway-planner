8aa4862eeb71e5f1309f488bf231492ac3fa6b21
Prbhvv <124075750+BlackJacob17@users.noreply.github.com>
2025-06-20 16:35:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://railway-planner-ten.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://railway-planner-frontend.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
